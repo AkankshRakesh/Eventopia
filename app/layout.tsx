c7b0d7ba9dc3db2d2b076131f8a6278b6b46848c
AkankshRakesh <akankshrakesh@gmail.com>
2024-07-03 19:17:02 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
+const poppins = Poppins({ subsets: ["latin"] , weight : ['400','500','600','700'], variable : '--font-poppins'});
+export const metadata: Metadata = {
+  title: "Eventopia",
+  description: "Event manager app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
