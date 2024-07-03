0a40e89cf63fed1a19f7ec656a60fa5fd89df01d
AkankshRakesh <akankshrakesh@gmail.com>
2024-07-03 21:08:57 +0530

    8th

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,21 @@
-// middleware.ts
-import { NextRequest, NextResponse } from 'next/server';
-import { getAuth } from '@clerk/nextjs/server';
-
-const publicRoutes = [
-  '/',
-  '/events/:id',
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-  '/api/uploadthing'
-];
-
-const ignoredRoutes = [
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-  '/api/uploadthing'
-];
-
-function isRouteMatched(pathname: string, routes: string[]): boolean {
-  return routes.some(route => {
-    const regex = new RegExp(route.replace(/:\w+/g, '[^/]+'));
-    return regex.test(pathname);
-  });
-}
-
-export default async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
-
-  if (isRouteMatched(pathname, publicRoutes) || isRouteMatched(pathname, ignoredRoutes)) {
-    return NextResponse.next();
-  }
-  
-
-  const auth = getAuth(req);
-
-  if (!auth.userId) {
-    // Redirect to the login page if the user is not authenticated
-    return NextResponse.redirect('/login');
-  }
-
-  // Proceed with the request if the user is authenticated
-  return NextResponse.next();
-}
-
+import { authMiddleware } from "@clerk/nextjs/server";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
