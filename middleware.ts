fd5962c669e9316b593fd1d6e348b31cc3448d53
AkankshRakesh <akankshrakesh@gmail.com>
2024-07-04 21:44:01 +0530

    15th

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isProtectedRoute = createRouteMatcher([
-'/',
-  '/api/webhooks(.*)',
-  '/api/uploadthing'
-])
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
-
+import { authMiddleware } from "@clerk/nextjs/server";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhooks/clerk',
+    '/api/webhooks/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhooks/clerk',
+    '/api/webhooks/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-
-}
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
