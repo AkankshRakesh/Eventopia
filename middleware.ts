370486e2c39797c5f5aa599af9660a8a091a7115
AkankshRakesh <akankshrakesh@gmail.com>
2024-07-04 22:39:44 +0530

    17th

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,11 @@ export default authMiddleware({
     '/api/webhooks/clerk',
     '/api/webhooks/stripe',
     '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhooks/clerk',
+    '/api/webhooks/stripe',
+    '/api/uploadthing'
   ]
 });
  
