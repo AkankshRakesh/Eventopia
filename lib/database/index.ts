5e0cea8dc34cabac07b06d8497a6a5e4bba2ebde
AkankshRakesh <akankshrakesh@gmail.com>
2024-07-04 14:41:02 +0530

    11th

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = "mongodb+srv://akankshrakesh:Akanksh01@eventopia.3wpbhav.mongodb.net/?retryWrites=true&w=majority&appName=Eventopia"
+const MONGODB_URI = process.env.MONGODB_URI; //mongodb+srv://akankshrakesh:Akanksh01@eventopia.3wpbhav.mongodb.net/?retryWrites=true&w=majority&appName=Eventopia
+console.log(MONGODB_URI);
 let cached = (global as any).mongoose || {conn : null, promise : null};
 
 export const connectToDatabase = async () => {
